eb2f2a0c18f68a588f5da34aba0e01dc298eafef
= <=>
2023-03-25 22:48:04 +0500

    commit

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+const stripe = require('stripe')('sk_test_51MjygHLX1AGuIT55yDhjtfwqlY4V3jKmwVhSFHk9q2cLDUDYfGGJBN2TdbTc9VyArI5IBDXFf5bkWpPMgCmANurp00PExQ18YN');
 
 exports.handler = async function (event, context) {
   if (event.body) {
